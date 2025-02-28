d1688b7bd3e3f00b53fadc1fb81d14e27c5ca943
Justin Asfour <6599551+justinasfour04@users.noreply.github.com>
2025-02-27 23:41:03 -0500

    Updated the build

--- db/index.ts
+++ db/index.ts
@@ -1,20 +1,18 @@
 import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
-// Create a connection pool
-export const pool = new Pool(
-  {
-    user: Deno.env.get("POSTGRES_USER"),
-    password: Deno.env.get("POSTGRES_PASSWORD"),
-    database: Deno.env.get("POSTGRES_DB"),
-    hostname: Deno.env.get("POSTGRES_HOST"),
-    port: Deno.env.get("POSTGRES_PORT"),
-  },
-  3,
-  true,
-);
-
 // Helper function to get a client from the pool
 export async function getClient() {
+  const pool = new Pool(
+    {
+      user: Deno.env.get("POSTGRES_USER"),
+      password: Deno.env.get("POSTGRES_PASSWORD"),
+      database: Deno.env.get("POSTGRES_DB"),
+      hostname: Deno.env.get("POSTGRES_HOST"),
+      port: Deno.env.get("POSTGRES_PORT"),
+    },
+    3,
+    true,
+  );
   const client = await pool.connect();
   return client;
 }
